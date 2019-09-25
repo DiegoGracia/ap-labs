99579a130d540628eceb5979279a6ded8e905806
Diego Armando Gracia Hinojosa <diego_gracia96@hotmail.com>
2019-09-24 21:43:52 -0500

    Update main.c

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+//Diego Armando Gracia Hinojosa
+//A01229716
 
-int main() {
-    return 0;
+#include <stdio.h>
+#include "strlib.c"
+
+int main(int argc, char **argv[]) {
+    if (argc == 4) {
+        int len = mystrlen(argv[1]);
+        char* add = mystradd(argv[1], argv[2]);
+        char* find = mystrfind(add, argv[3]) ? "yes":"no";
+        printf("Initial Length: %d\nNew String: %s\nSubString was found: %s\n", len, add, find);
+        return 0;
+    } else {
+        printf("U need to send the correct number of arguments...\n");
+        printf("How to run: ./main <original> <add> <find>\n");
+        return -1;
+    }
 }
