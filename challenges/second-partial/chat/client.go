2305d3ff91ebb8437189f9429f9c9ae4aa108b1d
Diego Gracia <A01229716@itesm.mx>
2019-10-27 23:40:42 -0600

    hola
    Signed-off-by: Diego Gracia <A01229716@itesm.mx>

--- challenges/second-partial/chat/client.go
+++ challenges/second-partial/chat/client.go
@@ -1,36 +1,38 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// See page 227.
-
-// Netcat is a simple read/write client for TCP servers.
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-//!+
+
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8000")
+	user := flag.String("user", "defaultUser", "User Name String")
+	server := flag.String("server", "localhost:9000", "Host address String")
+	flag.Parse()
+	conn, err := net.Dial("tcp", *server)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if blank := strings.TrimSpace(*user) == ""; blank {
+		log.Panic("No puede ser vacio el usuario")
+	}
+	io.WriteString(conn, "<user>"+*user+"\n")
 	done := make(chan struct{})
 	go func() {
-		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
-		log.Println("done")
-		done <- struct{}{} // signal the main goroutine
+		io.Copy(os.Stdout, conn)
+		log.Println("Desconectado")
+		done <- struct{}{}
 	}()
 	mustCopy(conn, os.Stdin)
 	conn.Close()
-	<-done // wait for background goroutine to finish
+	<-done
 }
-
-//!-
 
 func mustCopy(dst io.Writer, src io.Reader) {
 	if _, err := io.Copy(dst, src); err != nil {
--- challenges/second-partial/chat/server.go
+++ challenges/second-partial/chat/server.go
@@ -1,42 +1,46 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//Referencias:
+// Time Idea from: https://www.golangprograms.com/how-to-get-the-current-date-and-time-with-timestamp-in-local-and-other-timezones.html
+//// Time format from: https://gobyexample.com/time-formatting-parsing
 
-// See page 254.
-//!+
 
-// Chat is a server that lets clients chat with each other.
 package main
-
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"regexp"
+	"strings"
+	"time"
 )
-
-//!+broadcaster
-type client chan<- string // an outgoing message channel
+type client chan<- string
 
 var (
 	entering = make(chan client)
 	leaving  = make(chan client)
-	messages = make(chan string) // all incoming client messages
+	messages = make(chan string)
+	usersChan = make(map[string]net.Conn)
 )
 
+type clientData struct {
+  user string
+  admin bool
+}
+
+var listClients []clientData
+var ConUsers int = 0
+
 func broadcaster() {
-	clients := make(map[client]bool) // all connected clients
+	clients := make(map[client]bool)
 	for {
 		select {
 		case msg := <-messages:
-			// Broadcast incoming message to all
-			// clients' outgoing message channels.
 			for cli := range clients {
 				cli <- msg
 			}
-
 		case cli := <-entering:
 			clients[cli] = true
-
 		case cli := <-leaving:
 			delete(clients, cli)
 			close(cli)
@@ -44,44 +48,103 @@ func broadcaster() {
 	}
 }
 
-//!-broadcaster
-
-//!+handleConn
 func handleConn(conn net.Conn) {
-	ch := make(chan string) // outgoing client messages
+	ch := make(chan string)
+	who := ""
+	isAdmin := ConUsers == 0;
 	go clientWriter(conn, ch)
-
-	who := conn.RemoteAddr().String()
-	ch <- "You are " + who
-	messages <- who + " has arrived"
-	entering <- ch
-
 	input := bufio.NewScanner(conn)
 	for input.Scan() {
-		messages <- who + ": " + input.Text()
+		mssg := input.Text()
+		if match, _ := regexp.MatchString("<user>.+", mssg); match {
+			newClient := clientData{user: mssg, admin: isAdmin}
+			listClients = append(listClients, newClient)
+			ConUsers++
+			separatedString := strings.Split(mssg, ">")
+			who = separatedString[1]
+			fmt.Printf("irc-server > Nuevo usuario conectado [%s] \n", who)
+			ch <- "irc-server > bienvenido a IRC server"
+			ch <- "irc-server > Tu usuario  [" + who + "] se conecto de manera exitosa"
+			if isAdmin {
+				ch <- "irc-server > Felicidades eres el primer usuario"
+				ch <- "irc-server > Eres el nuevo Administrador"
+				fmt.Printf("irc-server > [%s] Fue promovido a Adminsitrador\n", who)
+			}
+			messages <- "irc-server > Nuevo usuario coenctado [" + who + "]"
+			entering <- ch
+			usersChan[who] = conn
+		} else if mssg == "/users" {
+			fmt.Fprintf(conn, "irc-server > ")
+			for key := range usersChan {
+				fmt.Fprintf(conn, "%s, ", key)
+			}
+			fmt.Fprintf(conn, "\n")
+		} else if match, _ := regexp.MatchString("^/msg .+ .+", mssg); match {
+			strimString := strings.Split(mssg, " ")
+			lenSlice := len(strimString)
+			if user, check := usersChan[strimString[1]]; check {
+				fmt.Fprintf(user, "%s [privateMSG] > ", who)
+				for i := 2; i < lenSlice; i++ {
+					fmt.Fprintf(user, " %s ", strimString[i])
+				}
+				fmt.Fprintf(user, "\n")
+			} else {
+				fmt.Fprintf(conn, "irc-server > No user [%s] found, use /users to see connected users. \n", strimString[1])
+			}
+		} else if mssg == "/time" {
+			loc, _ := time.LoadLocation("America/Mexico_city")
+    	tme := time.Now().In(loc).Format("15:04")
+    	fmt.Fprintf(conn, "irc-server > Local Time: %s %s\n", loc, tme)
+		} else if match, _ := regexp.MatchString("^/user .+$", mssg); match {
+			strimString := strings.Split(mssg, " ")
+			if user, check := usersChan[strimString[1]]; check {
+				fmt.Fprintf(conn, "irc-server > username: %s, IP: %s \n", strimString[1], user.RemoteAddr().String())
+			} else {
+				fmt.Fprintf(conn, "irc-server > No user (%s) found, use /users to see connected users. \n", strimString[1])
+			}
+		} else if match, _ := regexp.MatchString("^/kick .+$", mssg); match {
+			strimString := strings.Split(mssg, " ")
+			user := who
+			kickUser := strimString[1]
+			if listClients[0].user == user {
+				if listClients[0].admin {
+					fmt.Fprintf(conn, "irc-server > Kickeaste al usuario [%s]\n", kickUser)
+					fmt.Printf("irc-server > [%s] fue kickeado\n", kickUser)
+				} else {
+					fmt.Fprintf(conn, "irc-server > Fuiste kickeado de este canal\n")
+					fmt.Fprintf(conn, "irc-server > No se permite el mal uso del lenguaje  BAN-HAMMER\n")
+					usersChan[strimString[1]].Close()
+					delete(usersChan, kickUser)
+				}
+				messages <- "irc-server > [" + kickUser + "] El Admin se enfermo de poder y te correo"
+			} else {
+					ch <- "irc-server > Solo el administrador del canal puede hacer est accion"
+			}
+		} else {
+			messages <- who + "> " + input.Text()
+		}
 	}
-	// NOTE: ignoring potential errors from input.Err()
-
 	leaving <- ch
-	messages <- who + " has left"
+	messages <- "irc-server > [" + who + "] se fue"
+	fmt.Printf("irc-server > [%s] abandono \n", who)
 	conn.Close()
 }
 
 func clientWriter(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
-		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+		fmt.Fprintln(conn, msg)
 	}
 }
-
-//!-handleConn
-
-//!+main
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	host := flag.String("host", "localhost", "host string")
+	port := flag.String("port", "9000", "port string")
+	flag.Parse()
+	fmt.Printf("irc-server > El servidor inicio a %s:%s \n", *host, *port)
+	fmt.Printf("irc-server > Listo para recibir nuevos clientes \n")
+	listener, err := net.Listen("tcp", *host+":"+*port)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	go broadcaster()
 	for {
 		conn, err := listener.Accept()
@@ -92,5 +155,3 @@ func main() {
 		go handleConn(conn)
 	}
 }
-
-//!-main
