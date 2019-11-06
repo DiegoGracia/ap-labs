c13bd8c0860ed451fef61d06b57f675f53fd73fa
Diego Armando Gracia Hinojosa <diego_gracia96@hotmail.com>
2019-11-05 19:36:58 -0600

    Update main.c

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,19 +1,25 @@
-//Diego Armando Gracia Hinojosa
-//A01229716
-
 #include <stdio.h>
 #include "strlib.c"
 
-int main(int argc, char **argv[]) {
-    if (argc == 4) {
-        int len = mystrlen(argv[1]);
-        char* add = mystradd(argv[1], argv[2]);
-        char* find = mystrfind(add, argv[3]) ? "yes":"no";
-        printf("Initial Length: %d\nNew String: %s\nSubString was found: %s\n", len, add, find);
-        return 0;
-    } else {
-        printf("U need to send the correct number of arguments...\n");
-        printf("How to run: ./main <original> <add> <find>\n");
-        return -1;
+
+int main(int argc, char **argv){
+    switch(argc){
+        case 4:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+        if(mystrfind(argv[1], argv[3]) == 1){
+            printf("SubString was found : yes\n");
+        } else {
+            printf("SubString was found : no\n");
+        }
+        break;
+        case 3:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        printf("New String          : %s\n", mystradd(argv[1], argv[2]));
+        break;
+        case 2:
+        printf("Initial Length      : %d\n", mystrlen(argv[1]));
+        break;
     }
+    return 0;
 }
